100c34c07ab684db76dadca5b3a0afdff8ff5dd4
zhaochaokui <102526634+zhaochaokui@users.noreply.github.com>
2022-05-18 15:13:38 +0800

    Update leap.erl

--- exercises/practice/leap/src/leap.erl
+++ exercises/practice/leap/src/leap.erl
@@ -3,4 +3,17 @@
 -export([leap_year/1]).
 
 
-leap_year(_Year) -> undefined.
+%%leap_year(_Year) -> undefined.
+
+leap_year(Year) ->
+  case Year rem 4 =:= 0 andalso Year rem 100 =/= 0 of
+    true->
+      true;
+    false->
+      case Year rem 400 =:= 0 of
+        true-> true;
+        false->
+          false
+      end
+   end   
+  
