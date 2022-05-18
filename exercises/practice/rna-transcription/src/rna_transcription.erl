4ce5dd1381fa7a2468c4f831198daf65a3c2ca8b
zhaochaokui <102526634+zhaochaokui@users.noreply.github.com>
2022-05-18 15:49:36 +0800

    Update rna_transcription.erl

--- exercises/practice/rna-transcription/src/rna_transcription.erl
+++ exercises/practice/rna-transcription/src/rna_transcription.erl
@@ -3,4 +3,15 @@
 -export([to_rna/1]).
 
 
-to_rna(_Strand) -> undefined.
+%%to_rna(_Strand) -> undefined.
+
+to_rna(DNA)->
+  lists:foldr(fun($A,Acc)->
+                [$U|Acc];
+                ($T,Acc)->
+                [$A|Acc];
+                ($C,Acc)->
+                [$G|Acc];
+                ($G,Acc)->
+                [$c|Acc]
+              end).
