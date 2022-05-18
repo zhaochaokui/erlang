866fdd222fbf2fd2342ab7772ed0357ab7c9d7a6
zhaochaokui <102526634+zhaochaokui@users.noreply.github.com>
2022-05-18 15:36:23 +0800

    Update collatz_conjecture.erl

--- exercises/practice/collatz-conjecture/src/collatz_conjecture.erl
+++ exercises/practice/collatz-conjecture/src/collatz_conjecture.erl
@@ -3,4 +3,13 @@
 -export([steps/1]).
 
 
-steps(_N) -> undefined.
+%%steps(_N) -> undefined.
+
+steps(N) when is_integer(N)->
+  steps(N,0).
+steps(1,M)->
+  M;
+steps(N,M) when N rem 2 =:= 0 ->
+  steps(N/2,M+1);
+steps(N,M)->
+  steps(N*3+1,M+1).
