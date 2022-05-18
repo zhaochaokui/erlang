2494fdf04221161c9587f1112627127bbe2d6e02
zhaochaokui <102526634+zhaochaokui@users.noreply.github.com>
2022-05-18 16:41:59 +0800

    Update change.erl

--- exercises/practice/change/src/change.erl
+++ exercises/practice/change/src/change.erl
@@ -3,4 +3,13 @@
 -export([find_fewest_coins/2]).
 
 
-find_fewest_coins(_Target, _Coins) -> undefined.
+%%find_fewest_coins(_Target, _Coins) -> undefined.
+
+find_fewest_coins(Target,Coins)->
+  L = [E||E<-Coins,E =< Target],
+   find_fewest_coins(Target,L,[]).
+      
+find_fewest_coins(0,_,Acc)->
+  Acc;
+find_fewest_coins(Target,L,Acc)->
+   find_fewest_coins(Target - lists:max(L),lists:delete(lists:max(L),L),[lists:max(L)|Acc]).
