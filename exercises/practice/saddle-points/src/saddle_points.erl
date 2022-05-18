f3cd874dee78d78b2c126ef6537dd6b046520367
zhaochaokui <102526634+zhaochaokui@users.noreply.github.com>
2022-05-18 16:27:15 +0800

    Update saddle_points.erl

--- exercises/practice/saddle-points/src/saddle_points.erl
+++ exercises/practice/saddle-points/src/saddle_points.erl
@@ -2,5 +2,26 @@
 
 -export([saddle_points/1]).
 
-saddle_points(_Matrix) ->
-	undefined.
+-record(matrix,
+       {x,
+	y,
+	element
+       })
+
+%%saddle_points(_Matrix) ->
+%%	undefined.
+
+saddle_points(Matrix) when is_list(Matrix)->
+   saddle_points(Matrix,[]).
+
+saddle_points([],Matrix,Acc)->Acc;
+saddle_points([Point = #matrix{x = X, y= Y, element = E}|Rest],Matrix,Acc)->
+	L = [Ele||#matrix{x = Xl,element = Ele}<-Matrix,Xl=:=X],
+	L1 = [Ele||#matrix{y = Yl,element = Ele}<-Matrix,Yl=:=Y],
+	case E >= lists:max(L) andalso E =< lists:min(L1)  of
+		true->
+			saddle_points(Rest,Matrix,[Point|Acc]);
+		false->
+		   saddle_points(Rest,Matrix,Acc)
+	end.		     
+	
